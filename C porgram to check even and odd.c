7cd0efc9688b0fcf90c5a03f8de3852fe660885d
Alikhizar142 <alikhizar142@gmail.com>
2022-09-16 18:15:07 +0500

    C program to check the number is even or add

---
+++ C porgram to check even and odd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+ int a ;
+ printf("Enter a Number \n");
+scanf("%d",&a);
+if(a%2==0)
+{ 
+    printf("The number you enetred is even ");
+}
+else 
+printf("The number you entered is odd");
+return 0;
+}
+
