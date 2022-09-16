3d1a2dc43689130c36d982ac3648e40af5041f70
Alikhizar142 <alikhizar142@gmail.com>
2022-09-16 18:20:02 +0500

    C program  to find electercity bill

---
+++ To find electercity bill.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{ 
+    int unit;
+float amount, total_amount , sur_charge ;
+printf("Enter total Unit consumed ");
+scanf("%d",&unit);
+if(unit<=50)
+{
+    amount=unit*0.50 ;
+}
+else if(unit<=150)
+amount= 25 + ((unit-50)*0.75);
+else if(unit<=250)
+amount= 100 + ((unit-150)*1.20);
+else 
+amount= 220 + ((unit-250)*1.50);
+sur_charge=amount*0.20 ;
+total_amount=amount+sur_charge ;
+printf("Total cost of electercity is %.2f",total_amount);
+return 0 ;
+}
